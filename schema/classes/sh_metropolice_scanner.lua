ff153493893e988a74f7d6e430a6ec134ffe98ad
SEMCELT <semcelt02@gmail.com>
2025-09-23 15:55:15 -0300

    Scanner

--- schema/classes/sh_metropolice_scanner.lua
+++ schema/classes/sh_metropolice_scanner.lua
@@ -2,6 +2,10 @@ CLASS.name = "Metropolice Scanner"
 CLASS.description = "A metropolice scanner, it utilises Combine technology."
 CLASS.faction = FACTION_MPF
 
+function CLASS:OnSet(client)
+	client:SetModel("models/combine_scanner.mdl")
+end
+
 function CLASS:CanSwitchTo(client)
 	return Schema:IsCombineRank(client:Name(), "SCN") or Schema:IsCombineRank(client:Name(), "SHIELD")
 end
