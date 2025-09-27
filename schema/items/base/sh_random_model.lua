b07d927e18bcc4b49560813c6c1c0a10c483c901
TheLife <nicholastoya@yahoo.com.br>
2025-09-26 22:07:05 -0300

    Random Model Base Fix

--- schema/items/base/sh_random_model.lua
+++ schema/items/base/sh_random_model.lua
@@ -1,5 +1,3 @@
-
-
 ITEM.name = "Item"
 ITEM.model = "models/Gibs/HGIBS.mdl"
 ITEM.width = 1
@@ -8,13 +6,18 @@ ITEM.height = 1
 ITEM.models = {}
 
 function ITEM:OnInstanced(index, x, y, item)
-    self:SetData("model", self.models[math.random(#self.models)])
+    if (self.models and #self.models > 0) then
+        self:SetData("model", self.models[math.random(#self.models)])
+    end
 end
 
 function ITEM:GetModel()
-    local model = self:GetData("model")
-    if not model then
-        self:SetData("model", self.models[math.random(#self.models)])
+    local model = self:GetData("model") or self.model
+
+    if (not model and self.models and #self.models > 0) then
+        model = self.models[math.random(#self.models)]
+        self:SetData("model", model)
     end
-    return self:GetData("model") or self.model
+
+    return model or "models/Gibs/HGIBS.mdl"
 end
